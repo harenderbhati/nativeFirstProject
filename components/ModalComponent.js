fe17cb8f2cdda035fc067107071cb8afec415cdd
Nishant-Rajput21 <hawkeye18012102@gmail.com>
2024-12-30 14:31:22 +0530

    Add Modal in Our App with Show and Hide button and perform well

--- components/ModalComponent.js
+++ components/ModalComponent.js
@@ -1,18 +1,49 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, {useState} from 'react';
+import {Button, Modal, StyleSheet, Text, View} from 'react-native';
 
-const ModalComponent =()=>{
-    return(
-        <View>
-            <Text>Modal</Text>
+const ModalComponent = () => {
+  const [showModal, setShowModal] = useState(false);
+  return (
+    <View style={styles.main}>
+      <Modal transparent={true} visible={showModal} animationType='fade'>
+        <View style={styles.centredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello Harry!!!!!</Text>
+            <Button title="Close Modal" onPress={() => setShowModal(false)} />
+          </View>
         </View>
-    )
-}
+      </Modal>
+      <View style={styles.buttonView}>
+        <Button title="Show Modal" onPress={() => setShowModal(true)} />
+      </View>
+    </View>
+  );
+};
 
-const styles= StyleSheet.create({
-    main:{
-        flex:1
-    }
-})
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+  },
+  buttonView: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  centredView: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  modalView: {
+    backgroundColor: 'skyblue',
+    padding: 60,
+    borderRadius: 20,
+    shadowColor: 'blue',
+    elevation: 5,
+  },
+  modalText: {
+    fontSize: 30,
+    marginBottom: 30,
+  },
+});
 
-export default ModalComponent
+export default ModalComponent;
