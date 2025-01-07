06a1e0d827dab86112bc26a5bac9a41340296b11
Nishant-Rajput21 <hawkeye18012102@gmail.com>
2025-01-07 11:45:51 +0530

    pass data by using useState

--- components/Login.js
+++ components/Login.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
     Button,
     Text,
@@ -6,14 +7,17 @@ import {
   } from 'react-native';
 
 export const Login =(props)=>{
-    const name="Harry Thakur";
+    const [name, setName]=useState("")
+    // const name="Harry Thakur";
     const age=25;
   return(
     <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
       <Text style={{fontSize:30}}>
         Login  Screen
       </Text>
-      <TextInput placeholder='Enter your name' style={{borderRadius:5,borderColor:"red",borderWidth:5, margin:20}} />
+      <TextInput placeholder='Enter your name' style={{borderRadius:5,borderColor:"red",borderWidth:5, margin:20}}
+       onChangeText={(text)=>setName(text)}
+      />
       <Button title='Go to Home Page' onPress={()=>props.navigation.navigate("Home",{name,age})} />
     </View>
   )
