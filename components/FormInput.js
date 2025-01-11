7b7d98abe4ec9ede4ecf584bd610df3f338acf74
Nishant-Rajput21 <hawkeye18012102@gmail.com>
2025-01-11 14:06:56 +0530

    Add post Api to the inputFields

--- components/FormInput.js
+++ components/FormInput.js
@@ -1,44 +1,61 @@
-import { useState } from "react"
-import { Button, StyleSheet, Text, TextInput, View } from "react-native"
+import {useState} from 'react';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 
- 
+export const FormInput = () => {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState(0);
+  const [email, setEmail] = useState('');
 
- export const FormInput =()=>{
-    const [name,setName]= useState("")
-    const [age,setAge]= useState(0)
-    const [email,setEmail]= useState("")
+//    const getApiData =async()=>{
+//       const url="http://10.0.2.2:3000/users"
+//       let result =await fetch(url)
+//       result= await result.json()
+//       console.warn("result",result)
+//    }
+//    getApiData()
 
-    const saveData=async()=>{
-        console.warn("hello",name,age,email)
-        const url ="http://192.168.84.25:3000/users"
-        let result = await fetch(
-            url, {
-                method:"POST",
-                headers:{
-                    "Content-Type":"application/json"
-                },
-                body:JSON.stringify(name,age,email)
-            }
-        )
-    }
+  const saveData = async () => {
+    // console.warn('hello', name, age, email);
+    const url = 'http://10.0.2.2:3000/users';
+    let result = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({name, age, email}),
+    });
+  };
 
-    return(
-        <View>
-            <Text style={{fontSize:30}}>
-                Post Api with Input Feilds..
-            </Text>
-            <TextInput style={styles.input} value={name} onChangeText={(item)=>setName(item)} placeholder="enter your name"/>
-            <TextInput style={styles.input} value={age} onChangeText={(item)=>setAge(item)} placeholder="enter your age"/>
-            <TextInput style={styles.input} value={email} onChangeText={(item)=>setEmail(item)} placeholder="enter your email"/>
-            <Button title="Save Data"  onPress={saveData} />
-        </View>
-    )
- }
+  return (
+    <View>
+      <Text style={{fontSize: 30}}>Post Api with Input Feilds..</Text>
+      <TextInput
+        style={styles.input}
+        value={name}
+        onChangeText={text => setName(text)}
+        placeholder="enter your name"
+      />
+      <TextInput
+        style={styles.input}
+        value={age}
+        onChangeText={text => setAge(text)}
+        placeholder="enter your age"
+      />
+      <TextInput
+        style={styles.input}
+        value={email}
+        onChangeText={text => setEmail(text)}
+        placeholder="enter your email"
+      />
+      <Button title="Save Data" onPress={saveData} />
+    </View>
+  );
+};
 
- const styles= StyleSheet.create({
-    input:{
-        borderColor:"skyblue",
-        borderWidth:2,
-        margin:20
-    }
- })
+const styles = StyleSheet.create({
+  input: {
+    borderColor: 'skyblue',
+    borderWidth: 2,
+    margin: 20,
+  },
+});
